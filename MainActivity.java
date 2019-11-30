dfa27741f3a335ec1ff47e707950b16f7528149e
Riyabelle25 <55790848+Riyabelle25@users.noreply.github.com>
2019-12-01 01:02:43 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -70,7 +70,20 @@ import java.text.NumberFormat;
 
             // Use an intent to launch an email app.
             // Send the order summary in the email body.
-            
+                 int numberOfEmailsInInbox = 0;
+            int numberOfDraftEmails = 2;
+            String emailMessage = "You have " + numberOfEmailsInInbox + " emails. ";
+             String draftMessage = "You have " + numberOfDraftEmails + " email drafts.";
+             if (numberOfEmailsInInbox == 0) {
+            emailMessage = "You have no new messages. ";
+               }
+
+             if (numberOfDraftEmails == 0) {
+             draftMessage = "You have no new drafts.";
+              }
+
+             Log.v("InboxActivity", emailMessage);
+             Log.v("InboxActivity", draftMessage);
 
 
 
